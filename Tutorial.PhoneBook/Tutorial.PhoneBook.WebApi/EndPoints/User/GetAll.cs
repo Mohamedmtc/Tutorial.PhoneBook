919743f59700e85f52f4993f640fdf72a5f5cc3c
Mohamed Attia <eng.mohamed@hotmail.ca>
2023-02-25 18:58:30 +0200

    Endpoints

---
+++ Tutorial.PhoneBook/Tutorial.PhoneBook.WebApi/EndPoints/User/GetAll.cs
@@ -0,0 +1,22 @@
+﻿using Ardalis.ApiEndpoints;
+using Azure;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Tutorial.PhoneBook.WebApi.EndPoints.User
+{
+    public class GetAll : EndpointBaseSync.WithRequest<GetAllRequest>
+    .WithActionResult<GetAllResponse>
+    {
+        [HttpGet(GetAllRequest.Route)]
+        [SwaggerOperation(
+            Summary = "Get list of All Users",
+            Description = "list of All Users",
+            OperationId = "Users.GetAll",
+            Tags = new[]{ "UsersEndpoint" })]
+        public override ActionResult<GetAllResponse> Handle([FromQuery]GetAllRequest request)
+        {
+            return Ok();
+        }
+    }
+}
--- Tutorial.PhoneBook/Tutorial.PhoneBook.WebApi/Program.cs
+++ Tutorial.PhoneBook/Tutorial.PhoneBook.WebApi/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using Tutorial.PhoneBook.WebApi.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+    c.EnableAnnotations();
+});
 builder.Services.AddWebApilayerDependencyInjection(builder.Configuration);
 
 
