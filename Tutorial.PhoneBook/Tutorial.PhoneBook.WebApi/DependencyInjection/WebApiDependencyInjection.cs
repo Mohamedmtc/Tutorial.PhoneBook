81a7dcb403413745491aad8fdd03781f60a8d941
Mohamed Ebrahim Atia <eng.mohamed@hotmail.ca>
2023-03-19 09:30:50 +0200

    Latest

--- Tutorial.PhoneBook/Tutorial.PhoneBook.WebApi/DependencyInjection/WebApiDependencyInjection.cs
+++ Tutorial.PhoneBook/Tutorial.PhoneBook.WebApi/DependencyInjection/WebApiDependencyInjection.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Tutorial.PhoneBook.Application.Contracts;
+using Tutorial.PhoneBook.Application.DependencyInjection;
 using Tutorial.PhoneBook.Core.AppSetting;
 using Tutorial.PhoneBook.Core.DependencyInjection;
 using Tutorial.PhoneBook.Presistance.Context;
@@ -12,11 +14,19 @@ namespace Tutorial.PhoneBook.WebApi.DependencyInjection
         public static void AddWebApilayerDependencyInjection(this IServiceCollection services, IConfiguration configuration)
         {
             AddPreLayers(services, configuration);
-            AddDataBase(services);   
+            AddDataBase(services);
+            AddMapper(services);
         }
         private static void AddPreLayers(IServiceCollection services, IConfiguration configuration)
         {
             services.AddCoreLayer(configuration);
+            services.AddApplicationDependencyInjection(configuration);
+        }
+        public static void AddMapper(IServiceCollection services)
+        {
+            var mapper = new MapperConfiguration(cfg => cfg.AddMaps(typeof(WebApiDependencyInjection).Assembly)).CreateMapper();
+            services.AddSingleton(m => mapper);
+
         }
         private static void AddDataBase(IServiceCollection services)
         {
